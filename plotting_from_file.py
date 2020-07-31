f5c7798e683d2a4cc8281b69915470fdbb23a500
Pooja Dayaratna <poojadayaratna@gmail.com>
2020-07-31 17:44:17 -0500

    started chapter 20 exercises

--- plotting_from_file.py
+++ plotting_from_file.py
@@ -3,12 +3,15 @@ import csv
 
 x = []
 y = []
+y1 = []
 
 # with open('plotting_outputs/updated_varying_PID.csv', newline='') as csvfile:
 #     spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
 #     for row in spamreader:
 #         print(', '.join(row))
 #     print(row[1])
+
+fig, ax = plt.subplots(2)
 
 with open('data.csv', 'r') as csvfile:
     plots = csv.reader(csvfile, delimiter=',')
@@ -18,8 +21,11 @@ with open('data.csv', 'r') as csvfile:
         count += 1
         x.append(count)
         y.append(int(row[0]))
+        y1.append(int(row[1]))
 
-plt.plot(x, y, label='Loaded from file!')
+
+ax[0].plot(x, y, label='Loaded from file!')
+ax[1].plot(x, y1, label='second subplot')
 plt.xlabel('x')
 plt.ylabel('y')
 plt.title('Interesting Graph\nCheck it out')
--- plotting_with_subplots.py
+++ plotting_with_subplots.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 
-fig, ax = plt.subplots(2)
+fig, ax = plt.subplots(nrows=1, ncols=2)
 ax[0].plot([0, 1, 2, 3, 4], [0, 1, 2, 3, 4])
 ax[1].plot([0, 0.5, 1, 1.5, 2], [0, 1, 2, 3, 4], linestyle='dashed')
 plt.show()
